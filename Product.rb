91cdaaabcf6bf96e80ae17c922e0b14c8d80e25b
Tatchagon <o_k_t@hotmail.com>
2017-03-03 17:43:44 +0700

    delete some comment

--- Product.rb
+++ Product.rb
@@ -93,10 +93,6 @@ class Product
 
 end
 
-# a1 = Product.new('https://www.dropshoppingthai.com/product/2969/%E0%B9%80%E0%B8%81%E0%B8%A5%E0%B8%B5%E0%B8%A2%E0%B8%A7%E0%B8%96%E0%B8%99%E0%B8%AD%E0%B8%A1%E0%B8%AA%E0%B8%B2%E0%B8%A2%E0%B8%8A%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%95')
-# puts a1.sync_product
-# puts a1.code
-
 products = {
   "เกลียวถนอมสายชาร์ต": "https://www.dropshoppingthai.com/product/2969/%E0%B9%80%E0%B8%81%E0%B8%A5%E0%B8%B5%E0%B8%A2%E0%B8%A7%E0%B8%96%E0%B8%99%E0%B8%AD%E0%B8%A1%E0%B8%AA%E0%B8%B2%E0%B8%A2%E0%B8%8A%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%95",
   "ชุดถนอมสายชาร์จ คุมะ แบบที่3": "https://www.dropshoppingthai.com/product/10502/%E0%B8%8A%E0%B8%B8%E0%B8%94%E0%B8%96%E0%B8%99%E0%B8%AD%E0%B8%A1%E0%B8%AA%E0%B8%B2%E0%B8%A2%E0%B8%8A%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%88-%E0%B8%84%E0%B8%B8%E0%B8%A1%E0%B8%B0-%E0%B9%81%E0%B8%9A%E0%B8%9A%E0%B8%97%E0%B8%B5%E0%B9%883",
